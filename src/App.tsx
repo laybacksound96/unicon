61b7fd8920375763dd5fcf49de4bf94e2d4467eb
412 <laybacksound96@gmail.com>
2023-03-16 15:01:10 +0900

    INIT

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
